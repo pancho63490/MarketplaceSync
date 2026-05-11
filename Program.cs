d3c565428115d59ee0e67cc35d01e1c76132a900
Francisco Rodriguez <franciscorodriguez63490@gmail.com>
2026-05-10 20:38:16 -0600

    Run EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -3,20 +3,29 @@ using Microsoft.EntityFrameworkCore;
 using MarketplaceSync.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    builder.Services.AddScoped<MarketplaceDetectorService>();
+builder.Services.AddScoped<MarketplaceDetectorService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Apply database migrations automatically on startup.
+// This creates the tables in Render PostgreSQL if they do not exist.
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -31,6 +40,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
